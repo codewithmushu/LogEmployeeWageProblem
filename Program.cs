1eac1dab194dfe5e75fb1a9f5cc8e0159e3e302e
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-24 16:46:12 +0530

    [Muskan] Added : Created instance of Monthly wage problem.

--- Program.cs
+++ Program.cs
@@ -53,31 +53,54 @@ namespace assignment_no__8
 
 
 
-            Console.WriteLine("Enter employee name:");
-            string name = Console.ReadLine();
+            //Console.WriteLine("Enter employee name:");
+            //string name = Console.ReadLine();
 
-            CaseStatement emp = new CaseStatement(name);
+            //CaseStatement emp = new CaseStatement(name);
 
-            Console.WriteLine("Is the employee full-time or part-time? (F/P)");
-            char type = char.Parse(Console.ReadLine());
+            //Console.WriteLine("Is the employee full-time or part-time? (F/P)");
+            //char type = char.Parse(Console.ReadLine());
 
-            switch (type)
-            {
-                case 'F':
-                case 'f':
-                    Console.WriteLine("Sorry, full-time employees are not supported in this program.");
-                    break;
-                case 'P':
-                case 'p':
-                    Console.WriteLine("Enter the number of hours worked:");
-                    int hours = int.Parse(Console.ReadLine());
-                    emp.SetHoursWorked(hours);
-                    emp.CalculateWage();
-                    break;
-                default:
-                    Console.WriteLine("Invalid employee type.");
-                    break;
-            }
+            //switch (type)
+            //{
+            //    case 'F':
+            //    case 'f':
+            //        Console.WriteLine("Sorry, full-time employees are not supported in this program.");
+            //        break;
+            //    case 'P':
+            //    case 'p':
+            //        Console.WriteLine("Enter the number of hours worked:");
+            //        int hours = int.Parse(Console.ReadLine());
+            //        emp.SetHoursWorked(hours);
+            //        emp.CalculateWage();
+            //        break;
+            //    default:
+            //        Console.WriteLine("Invalid employee type.");
+            //        break;
+            //}
+
+            //// Wait for user input before closing the console window
+            //Console.ReadKey();
+
+
+
+            Console.WriteLine("Enter full-time employee name:");
+            string ftName = Console.ReadLine();
+            MonthlyWage ftEmp = new MonthlyWage(ftName);
+
+            Console.WriteLine("Enter part-time employee name:");
+            string ptName = Console.ReadLine();
+            MonthlyWage ptEmp = new MonthlyWage(ptName);
+
+            Console.WriteLine("Enter the number of hours worked by {0}:", ftName);
+            int ftHours = int.Parse(Console.ReadLine());
+            ftEmp.SetHoursWorked(ftHours);
+            ftEmp.CalculateMonthlyWage();
+
+            Console.WriteLine("Enter the number of hours worked by {0}:", ptName);
+            int ptHours = int.Parse(Console.ReadLine());
+            ptEmp.SetHoursWorked(ptHours);
+            ptEmp.CalculateMonthlyWage();
 
             // Wait for user input before closing the console window
             Console.ReadKey();
