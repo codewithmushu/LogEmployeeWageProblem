f262162a4a1cdfcd107f73962d899d4b1803a957
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-24 13:03:11 +0530

    [Muskan] Added : Greetings for logging employee wage problem.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_no__8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the log employee wage problem");
+        }
+    }
+}
