dc53825f0cdb6f07d3c6bf72326c76cac000a7b5
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-24 16:18:50 +0530

    [Muskan] Added : Casestatement, instances, instance variables and methods are used.

---
+++ CaseStatement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_no__8
+{
+    class CaseStatement
+    {
+        // Instance variables
+        private string name;
+        private int hoursWorked;
+
+        // Constructor
+        public CaseStatement(string name)
+        {
+            this.name = name;
+        }
+
+        // Method to set the hours worked
+        public void SetHoursWorked(int hours)
+        {
+            hoursWorked = hours;
+        }
+
+        // Method to calculate and print the wage
+        public void CalculateWage()
+        {
+            int wage = hoursWorked * 4;  // Part time wage is assumed to be 4 per hour
+            Console.WriteLine("{0} worked for {1} hours and earned ${2}", name, hoursWorked, wage);
+        }
+    }
+}
