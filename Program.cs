22df9f52e928f826918c31cd31e3322971edd619
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-24 15:44:56 +0530

    [Muskan] Added : Created Instance of part time employee wage.

--- Program.cs
+++ Program.cs
@@ -23,17 +23,37 @@ namespace assignment_no__8
 
 
 
-            // Create an instance of the Employee class
-            DailyWage emp = new DailyWage();
+            //// Create an instance of the Employee class
+            //DailyWage emp = new DailyWage();
 
-            // Set the number of hours worked
-            emp.SetHoursWorked(8);
+            //// Set the number of hours worked
+            //emp.SetHoursWorked(8);
 
-            // Display the daily wage
-            emp.DisplayWage();
+            //// Display the daily wage
+            //emp.DisplayWage();
+
+            //// Wait for user input before closing the console window
+            //Console.ReadKey();
+
+
+
+            // Create an instance of the Employee class for full-time employee
+            PartTimeEmpWage empFullTime = new PartTimeEmpWage();
+            empFullTime.SetHoursWorked(8);
+            empFullTime.DisplayWage();
+
+            // Create an instance of the Employee class for part-time employee
+            PartTimeEmpWage empPartTime = new PartTimeEmpWage();
+            empPartTime.SetHoursWorked(4);
+            empPartTime.SetIsPartTime(true);
+            empPartTime.DisplayWage();
 
             // Wait for user input before closing the console window
             Console.ReadKey();
+
+
+
+
         }
     }
 }
